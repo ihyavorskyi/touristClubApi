5d77cedee766b1dcf13e7a551bdb21827282b61d
ihyavorskyi <ihor201324@gmail.com>
2020-12-13 15:21:15 +0200

    added setting to cabinet

--- ClientSide/src/app/modules/account/cabinet/cabinet.component.ts
+++ ClientSide/src/app/modules/account/cabinet/cabinet.component.ts
@@ -1,7 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {AccountService} from '../services/account.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../services/account.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/data/models/user';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ChangePasswordFormComponent } from '../change-password-form/change-password-form.component';
 
 @Component({
   selector: 'app-cabinet',
@@ -16,17 +19,39 @@ export class CabinetComponent implements OnInit {
   public response: { dbPath: '' };
   timeStamp: number;
 
+  pageFormSetting: FormGroup;
+  isFormEnabled = false;
+  isDialogOpen = false;
+
+
   constructor(private fb: FormBuilder,
-              private accountService: AccountService) {
+    private accountService: AccountService,
+    private fbSetting: FormBuilder,
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog) {
     const uId = localStorage.getItem('uId');
     this.accountService.getUser(uId).subscribe(value => {
       console.log('user got');
       this.user = value;
       this.photoPath = `https://localhost:5001/api/account/avatar/${this.user.id}`;
+      this.initFormSet();
     });
   }
 
   ngOnInit(): void {
+  }
+
+  
+  initFormSet(): void {
+    this.pageForm = this.fb.group({
+      email: [this.user.email],
+      firstName: [this.user.firstName],
+      lastName: [this.user.lastName],
+      age: [this.user.age],
+      userName: [this.user.userName],
+      phoneNumber: [this.user.phoneNumber]
+    });
+    this.pageForm.disable();
   }
 
   initForm(): void {
@@ -57,6 +82,61 @@ export class CabinetComponent implements OnInit {
 
   public uploadFinished = (event) => {
     this.setLinkPicture(this.photoPath);
+  }
+
+  onSubmit(): void {
+    this.pageForm.disable();
+    const model: User = {
+      email: this.pageForm.get('email').value,
+      firstName: this.pageForm.get('firstName').value,
+      lastName: this.pageForm.get('lastName').value,
+      age: Number(this.pageForm.get('age').value),
+      userName: this.pageForm.get('userName').value,
+      phoneNumber: this.pageForm.get('phoneNumber').value,
+      id: this.user.id,
+      role: null
+    };
+    this.accountService.updateUser(model).subscribe(value => {
+      if (value) {
+        this.user = model;
+        this.snackBar.open('Зміни збережено', 'Подякував', {
+          duration: 3000,
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+          panelClass: ['my-snack'],
+          politeness: 'assertive'
+        });
+      }
+    });
+    this.isFormEnabled = !this.isFormEnabled;
+  }
+
+  toogleForm(): void {
+    this.isFormEnabled = !this.isFormEnabled;
+    this.isFormEnabled === true ? this.pageForm.enable() : this.pageForm.disable();
+  }
+
+  changePassword(): void {
+    let dialogRef;
+    dialogRef = this.dialog.open(ChangePasswordFormComponent, {
+      width: '450px',
+      panelClass: 'my-dialog-window'
+    });
+    dialogRef.afterOpened().subscribe(res => {
+      this.isDialogOpen = true;
+    });
+    dialogRef.afterClosed().subscribe((value) => {
+      this.isDialogOpen = false;
+      if (value.success) {
+        this.snackBar.open('Пароль змінено', 'Подякував', {
+          duration: 3000,
+          horizontalPosition: 'center',
+          verticalPosition: 'bottom',
+          panelClass: ['my-snack'],
+          politeness: 'assertive'
+        });
+      }
+    });
   }
 
 
